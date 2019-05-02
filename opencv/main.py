e479dab3096def1fbad504e4141df9f3d62ef321
Antônio <antonio.fernades.bsi@gmail.com>
2019-05-02 16:36:43 -0300

    Conseguindo pausar o vídeo em determinado frame

--- opencv/main.py
+++ opencv/main.py
@@ -1,35 +1,50 @@
 import numpy as np
 import cv2 as cv
-drawing = False # true if mouse is pressed
-mode = True # if True, draw rectangle. Press 'm' to toggle to curve
-ix,iy = -1,-1
-# mouse callback function
-def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing,mode
-    if event == cv.EVENT_LBUTTONDOWN:
-        drawing = True
-        ix,iy = x,y
-    elif event == cv.EVENT_MOUSEMOVE:
-        if drawing == True:
-            if mode == True:
-                cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-            else:
-                cv.circle(img,(x,y),5,(0,0,255),-1)
-    elif event == cv.EVENT_LBUTTONUP:
-        drawing = False
-        if mode == True:
-            cv.rectangle(img,(ix,iy),(x,y),(0,255,0),-1)
-        else:
-            cv.circle(img,(x,y),5,(0,0,255),-1)
-            
-img = np.zeros((512,512,3), np.uint8)
-cv.namedWindow('image')
-cv.setMouseCallback('image',draw_circle)
-while(1):
-    cv.imshow('image',img)
-    k = cv.waitKey(1) & 0xFF
-    if k == ord('m'):
-        mode = not mode
-    elif k == 27:
-        break
+import sys
+
+cap = cv.VideoCapture('videos/teste.mp4')
+
+font = cv.FONT_HERSHEY_SIMPLEX
+color = (255, 255, 255)
+while cap.isOpened():
+	ret, frame = cap.read()
+
+	if not ret:
+		print("Can't receive frame (stream end?). Exiting ...")
+		break
+
+	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	
+	frameAtual = cap.get(1)
+	cv.putText(gray, str(frameAtual), (0,17), font, 0.5, color, 2, cv.LINE_AA)
+
+	cv.imshow('frame', gray)
+
+	key = cv.waitKey(25)
+	if key == ord('q'):
+		break
+	elif key == ord('p'):
+		while 1:
+			ret, frame = cap.read()
+
+			gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+			cv.putText(gray, str(frameAtual), (0,17), font, 0.5, color, 2, cv.LINE_AA)
+
+			cv.imshow('frame', gray)
+			key2 = cv.waitKey(0)
+			if key2 == ord('p'):
+				break
+			elif key2 == ord('q'):
+				sys.exit()
+			elif key2 == ord('k'):
+				cap.set(1, frameAtual-1)
+				frameAtual = cap.get(1)
+				cv.putText(gray, str(frameAtual), (0,17), font, 0.5, color, 2, cv.LINE_AA)
+			else:
+				cap.set(1, frameAtual+1)
+				frameAtual = cap.get(1)
+				cv.putText(gray, str(frameAtual), (0,17), font, 0.5, color, 2, cv.LINE_AA)
+
+cap.release()
 cv.destroyAllWindows()
