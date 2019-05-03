d5afc4659cda5ef23e830849520edeb0ebb14c61
Antônio <antonio.fernades.bsi@gmail.com>
2019-05-03 16:46:46 -0300

    Trabalhar com imagens agora

--- opencv/imgs/img.py
+++ opencv/imgs/img.py
@@ -1,15 +1,20 @@
 import numpy as np
 import cv2 as cv
 
-# BGR instead of RGB
-# img[y,x]
-img = cv.imread('img1.png')
-opencv = img[115:144, 85:200]
-img[3:32, 85:200] = opencv
-cv.imshow('img', img)
+car = cv.imread('car.jpg')
+road = cv.imread('road.jpg')
+
+car2gray = cv.cvtColor(car, cv.COLOR_BGR2GRAY)
+
+dst = cv.addWeighted(car,0.5, road,1, 0)
+
+ret, threshold = cv.threshold(car2gray, 5, 255, cv.THRESH_BINARY)
+
+cv.imshow('threshold', threshold)
+cv.imshow('car2gray',car2gray)
+cv.imshow('car', car)
+cv.imshow('road', road)
+cv.imshow('dst', dst)
+
 cv.waitKey()
 cv.destroyAllWindows()
-# print(img[52,128])
-# print(img.shape)
-# print(img.size)
-# print(img.dtype)
