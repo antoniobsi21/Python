a6379a8044102b54498fe8a31826431f613142f0
Antônio Fernandes <antonio.fernandes.bsi@gmail.com>
2019-05-05 21:33:46 -0300

    Transição

--- opencv/imgs/main.py
+++ opencv/imgs/main.py
@@ -1,31 +1,43 @@
 import numpy as np
 import cv2 as cv
 
-img1 = cv.imread('main.png')
+img1 = cv.imread('fundo.png')
 img2 = cv.imread('logo.png')
+img2 = cv.resize(img2[0:175, :], None, fx=0.6, fy=0.6, interpolation = cv.INTER_CUBIC)
 
 rows1, cols1, channels1 = img1.shape
 rows2, cols2, channels2 = img2.shape
-roi = img1[rows1-rows2:rows1, 0:cols2]
+contC = 0
+going = True
 
 img2gray = cv.cvtColor(img2, cv.COLOR_BGR2GRAY)
-ret, mask = cv.threshold(img2gray, 150, 255, cv.THRESH_BINARY)
+ret, mask = cv.threshold(img2gray, 20, 255, cv.THRESH_BINARY)
 mask_inv = cv.bitwise_not(mask)
 
-roi_bg = cv.bitwise_and(roi,roi,mask=mask)
+img2_logo = cv.bitwise_and(img2, img2, mask=mask)
 
-img2_logo = cv.bitwise_and(img2, img2, mask=mask_inv)
+while 1:
+	result = img1.copy()
+	if cols2 + contC > cols1:
+		going = False
+		contC -=1
+	elif contC < 0:
+		going = True
+		contC += 1
+	roi = img1[rows1-rows2:rows1, contC:cols2+contC]
 
-dst = cv.add(roi_bg, img2_logo)
-img1[rows1-rows2:rows1, 0:cols2] = dst
+	roi_bg = cv.bitwise_and(roi,roi,mask=mask_inv)
 
-cv.imshow('img2', img2)
-cv.imshow('img2gray', img2gray)
-cv.imshow('maskimg2gray', mask)
-cv.imshow('mask_inv', mask_inv)
-cv.imshow('img1_bg', roi_bg)
-cv.imshow('img2_fg', img2_logo)
-cv.imshow('result', img1)
+	dst = cv.add(roi_bg, img2_logo)
+	result[rows1-rows2:rows1, contC:cols2+contC] = dst
 
-cv.waitKey(0)
+	cv.imshow('result', result)
+	key = cv.waitKey(1)
+	if key == ord('q'):
+		break
+	if going:
+		contC += 1
+	else:
+		contC -= 1
+
 cv.destroyAllWindows()
